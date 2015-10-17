7a16dce1640675758303eabdd4ae787441dd95c9
Dr Nic Williams <drnicwilliams@gmail.com>
2015-10-17 15:52:06 -0700

    delete service keys before deleting service

--- ci/tasks/create-sql-service.sh
+++ ci/tasks/create-sql-service.sh
@@ -6,6 +6,11 @@ cf login -a https://api.run.pivotal.io -u ${cf_username} -p ${cf_password} -o ${
 
 set -x # print commands
 
+service_keys=$(cf curl $(cf curl "/v2/spaces/$(cat ~/.cf/config.json| jq -r .SpaceFields.Guid)/service_instances?q=name:${cf_service_name}" | jq -r ".resources[0].entity.service_keys_url") | jq -r ".resources[].metadata.url")
+for key in $service_keys; do
+  cf curl -X DELETE ${key}
+done
+
 cf ds -f ${cf_service_name}
 cf cs elephantsql turtle ${cf_service_name}
 cf create-service-key ${cf_service_name} ${cf_service_name}-key
