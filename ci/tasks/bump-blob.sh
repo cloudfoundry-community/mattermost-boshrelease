72b27db92db6f8d177b5cc52e1cd2f77515f4c39
Dr Nic Williams <drnicwilliams@gmail.com>
2015-10-16 17:04:53 -0700

    update blob

--- ci/tasks/bump-blob.sh
+++ ci/tasks/bump-blob.sh
@@ -15,16 +15,21 @@ blobstore:
     secret_access_key: ${aws_secret_access_key}
 EOF
 
-set -x # print commands
+if [[ ! -f tmp/mattermost-platform/mattermost.tar.gz ]]; then
+  echo "Expected file tmp/mattermost-platform/mattermost.tar.gz"
+  exit 1
+fi
 
-ls -ral tmp/mattermost-platform/*
+version=$(cat tmp/mattermost-platform/version)
+cp tmp/mattermost-platform/mattermost.tar.gz tmp/mattermost-${version}.tar.gz
+bosh add blob tmp/mattermost-${version}.tar.gz mattermost
 
-# bosh -n upload blobs
-#
-# if [[ -z "$(git config --global user.name)" ]]
-# then
-#   git config --global user.name "Concourse Bot"
-#   git config --global user.email "drnic+bot@starkandwayne.com"
-# fi
-#
-# git commit -a -m "updated image layers"
+bosh -n upload blobs
+
+if [[ -z "$(git config --global user.name)" ]]
+then
+  git config --global user.name "Concourse Bot"
+  git config --global user.email "drnic+bot@starkandwayne.com"
+fi
+
+git commit -a -m "updated mattermost v${version}"
